8bd1677b29161dafe26872f7040cec14d2f4611f
mattmuirhead <matt.rob.muirhead@googlemail.com>
2019-05-15 16:36:49 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,35 +8,36 @@
  * https://flickity.metafizzy.co
  * Copyright 2015-2019 Metafizzy
  */
+if (typeof window !== `undefined`) {
+  ( function( window, factory ) {
+    // universal module definition
+    /* jshint strict: false */
+    if ( typeof define == 'function' && define.amd ) {
+      // AMD
+      define( [
+        './flickity',
+        './drag',
+        './prev-next-button',
+        './page-dots',
+        './player',
+        './add-remove-cell',
+        './lazyload'
+      ], factory );
+    } else if ( typeof module == 'object' && module.exports ) {
+      // CommonJS
+      module.exports = factory(
+        require('./flickity'),
+        require('./drag'),
+        require('./prev-next-button'),
+        require('./page-dots'),
+        require('./player'),
+        require('./add-remove-cell'),
+        require('./lazyload')
+      );
+    }
 
-( function( window, factory ) {
-  // universal module definition
-  /* jshint strict: false */
-  if ( typeof define == 'function' && define.amd ) {
-    // AMD
-    define( [
-      './flickity',
-      './drag',
-      './prev-next-button',
-      './page-dots',
-      './player',
-      './add-remove-cell',
-      './lazyload'
-    ], factory );
-  } else if ( typeof module == 'object' && module.exports ) {
-    // CommonJS
-    module.exports = factory(
-      require('./flickity'),
-      require('./drag'),
-      require('./prev-next-button'),
-      require('./page-dots'),
-      require('./player'),
-      require('./add-remove-cell'),
-      require('./lazyload')
-    );
-  }
-
-})( window, function factory( Flickity ) {
-  /*jshint strict: false*/
-  return Flickity;
-});
+  })( window, function factory( Flickity ) {
+    /*jshint strict: false*/
+    return Flickity;
+  });
+}
